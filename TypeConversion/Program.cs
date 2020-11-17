7835c227eaa07533baecb8ffdacafb9894012645
Christopher <christopherse@live.com>
2020-11-17 20:58:12 +0100

    Casting and Type Conversion - ToString()

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int serialNo = 100;
+            string accountNo = ""; //empty as we created it
+
+            accountNo = serialNo.ToString(); // set the accountnumber to serialNo and ToString converts any number to a string
+
+            Console.WriteLine(accountNo); //Now it's 100
+        }
+    }
+}
