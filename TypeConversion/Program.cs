bdf30596edeb90d707a72b9d3067529c79fa5e25
Christopher <christopherse@live.com>
2020-11-17 22:08:43 +0100

    Convert.ToInt32();

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -8,10 +8,15 @@ namespace TypeConversion
         {
             int serialNo = 100;
             string accountNo = ""; //empty as we created it
+            float anyNo = 3.5f; // f = floating point
 
             accountNo = serialNo.ToString(); // set the accountnumber to serialNo and ToString converts any number to a string
 
             Console.WriteLine(accountNo); //Now it's 100
+
+            serialNo = Convert.ToInt32(anyNo); //Convert float to int32
+
+            Console.WriteLine(serialNo); // Round and convert to 4, int does not handle floating points
         }
     }
 }
