e470678fc5430b90e4fdabb49d8319f6ebc08531
Christopher <christopherse@live.com>
2020-11-18 09:37:52 +0100

    Primitive datatypes

---
+++ DataTypesInDepth/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataTypesInDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primitive datatypes
+
+            int xMax = int.MaxValue; // Max value (int) 2,147,483,647
+
+            int xMin = int.MinValue; // Min value (int) -2,147,483,647
+
+            Console.WriteLine(xMax);
+
+            Console.WriteLine(xMin);
+
+            uint y = 2147483648; // Convert to uint in order to take higher max-value
+
+            Console.WriteLine(y);
+
+            uint zMax = uint.MaxValue; //Max value for (unit) 4,294,967,295
+
+            uint zMin = uint.MinValue; //Min value for (unit) 0
+
+            Console.WriteLine(zMax);
+
+            Console.WriteLine(zMin);
+        }
+    }
+}
