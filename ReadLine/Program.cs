27c20ec5787dc13298df7e4339adf3835c2d3738
statecs <statec@icloud.com>
2020-12-07 16:38:42 +0100

    ReadLine

---
+++ ReadLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            Console.ReadLine();
+        }
+    }
+}
