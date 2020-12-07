c6da160f4b12786f8d06a6f89b4734a6cd223660
Christopher <christopherse@live.com>
2020-12-07 14:19:32 +0100

    DateTime.Today and DateTime.Now

--- DateTimeLecture/Program.cs
+++ DateTimeLecture/Program.cs
@@ -8,8 +8,11 @@ namespace DateTimeLecture
         {
             DateTime myDateUnassigned = new DateTime(); //Default value of time. Did not assign any value
             DateTime myDate = new DateTime(1999,10,5); //Assigned value of year 1999, Month October and Day 5.
-           
-            Console.WriteLine("Date {0}, {1}", myDateUnassigned, myDate );
+
+            DateTime todayDate = DateTime.Today; //Today date without time
+            DateTime todayDateTime = DateTime.Now; //Today date with current time
+
+            Console.WriteLine("Date {0}, {1}, {2}, {3}", myDateUnassigned, myDate, todayDate, todayDateTime);
         }
     }
 }
