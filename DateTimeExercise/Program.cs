3f3bf50f28b4882259379f549367dd481885fc02
statecs <statec@icloud.com>
2020-12-07 15:50:28 +0100

    DateTime exercise Solution 2

--- DateTimeExercise/Program.cs
+++ DateTimeExercise/Program.cs
@@ -28,6 +28,30 @@ namespace DateTimeExercise
             string thirdFormattedDateYear = string.Format("{0:yyyy}", todayDate);
             Console.WriteLine("\n Day {0} \n Month {1} \n Year {2}", thirdFormattedDateDay, thirdFormattedDateMonth, thirdFormattedDateYear);
 
+            /*            */
+            /* Solution 2 */
+            /*            */
+
+            DateTime mydate = DateTime.Now;
+            string formattedDated = "";
+            //First Format: 30-09-2020 01:15:55
+
+            formattedDated = string.Format("{0:dd-MM-yyyy HH:mm:ss}", mydate);
+            Console.WriteLine(formattedDated);
+
+            //Second Format : Saturday of month September 2020
+            formattedDated = string.Format("{0:dddd 'of month' MMMM 'year' yyyy}", mydate);
+            Console.WriteLine(formattedDated);
+
+            /* Third Format
+            * Day Saturday
+            * Month September
+            * Year 2020
+            */
+
+            formattedDated = string.Format("{0:'Day' dddd \n'Month' MMMM \n'Year' yyyy}", mydate); // \n escape sequences
+            Console.WriteLine(formattedDated);
+
         }
     }
 }
