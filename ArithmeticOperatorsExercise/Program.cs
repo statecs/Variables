a2f6faf780f755b6786bd1b8b0a537af17a2964f
statecs <statec@icloud.com>
2020-12-07 17:06:35 +0100

    ArithmeticOperatorsExercise

---
+++ ArithmeticOperatorsExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArithmeticOperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+
+            int result = 0;
+
+            result = x + y;
+            Console.WriteLine("" + x + " + " + y + " = " + result); //Concatenate
+            Console.WriteLine("{0} + {1} = {2}", x, y, result); //Placeholder
+           
+            result = x - y;
+            Console.WriteLine("{0} - {1} = {2}", x, y, result);
+
+            result = x * y;
+            Console.WriteLine("{0} * {1} = {2}", x, y, result);
+
+            result = x / y;
+            Console.WriteLine("{0} / {1} = {2}", x, y, result);
+
+            result = x % y;
+            Console.WriteLine("{0} % {1} = {2}", x, y, result);
+
+
+        }
+    }
+}
