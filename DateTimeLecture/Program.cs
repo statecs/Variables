c6c472db6ea078e578d98982e649de726944f320
Christopher <christopherse@live.com>
2020-12-07 14:13:40 +0100

    DateTime datatype

---
+++ DateTimeLecture/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateTimeLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDateUnassigned = new DateTime(); //Default value of time. Did not assign any value
+            DateTime myDate = new DateTime(1999,10,5); //Assigned value of year 1999, Month October and Day 5.
+           
+            Console.WriteLine("Date {0}, {1}", myDateUnassigned, myDate );
+        }
+    }
+}
