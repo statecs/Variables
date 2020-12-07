40e799dfbc30a96d2c63e3e5f82d0f7d3553aa21
statecs <statec@icloud.com>
2020-12-07 16:00:17 +0100

    ErrorTypes

---
+++ ErrorsTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ErrorsTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            /* a = ""; b = 1; c = 0; //Trying to assign a string to integer. Syntax error */
+
+            /* a = 0 b = 1; c = 0; //Violation of the rules. Syntax error */
+
+            a = 0; b = 1; c = 0;
+            c = b / a;
+
+            Console.WriteLine(c); // Unhandled Exception. Attempted to divide by zero. 
+
+        }
+    }
+}
